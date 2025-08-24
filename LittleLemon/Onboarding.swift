81ceb375bb52bfa7ba18801321baee43a6b06521
sakersun <sakersun@163.com>
2025-08-24 23:12:31 +0800

    new code

--- LittleLemon/Onboarding.swift
+++ LittleLemon/Onboarding.swift
@@ -11,12 +11,12 @@ let kFirstName = "key first name"
 let kLastName = "key last name"
 let kEmail = "key email"
 let kIsLoggedIn = "key is logged in"
-let kName = "key name"
 
 struct Onboarding: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
     @State private var email: String = ""
     @State private var isLoggedIn: Bool = false
-    @State private var name: String = ""
     
     var body: some View {
         NavigationStack {
@@ -60,14 +60,16 @@ struct Onboarding: View {
                 .background(
                     Color(red: 75 / 255, green: 90 / 255, blue: 82 / 255)
                 )
-                Text("Name *").frame(maxWidth: .infinity, alignment: .leading).foregroundColor(.gray)
-                TextField("Name", text: $name)
+                Text("First Name *").frame(maxWidth: .infinity, alignment: .leading).foregroundColor(.gray)
+                TextField("First Name", text: $firstName)
+                Text("Last Name *").frame(maxWidth: .infinity, alignment: .leading).foregroundColor(.gray)
+                TextField("Last Name", text: $lastName)
                 Text("Email *").frame(maxWidth: .infinity, alignment: .leading).foregroundColor(.gray)
                 TextField("Email", text: $email)
 
                 Spacer()
                 Button("Register") {
-                    if name.isEmpty || email.isEmpty {
+                    if firstName.isEmpty || lastName.isEmpty || email.isEmpty {
                         print("register failed")
                         return
                     }
@@ -76,7 +78,8 @@ struct Onboarding: View {
                         return
                     }
                     isLoggedIn = true
-                    UserDefaults.standard.set(name, forKey: kName)
+                    UserDefaults.standard.set(firstName, forKey: kFirstName)
+                    UserDefaults.standard.set(lastName, forKey: kLastName)
                     UserDefaults.standard.set(email, forKey: kEmail)
                     UserDefaults.standard.set(true, forKey: kIsLoggedIn)
                 }
